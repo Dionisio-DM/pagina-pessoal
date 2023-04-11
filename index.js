7e8d0a58ac93f19457c76aaad86a87ba61fead23
Dionisio Diogenes <dionisiodiogenes@hotmail.com>
2023-04-11 17:14:56 -0300

    Adicionando interação à página usando js

---
+++ index.js
@@ -0,0 +1,55 @@
+const cardBtn = document.getElementById("card-btn");
+const card = document.querySelector(".card")[0];
+
+const submitForm = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+const nameRegex = /^(?!.*\s{2,})\b[a-zA-ZÀ-ú]+(?:\s[a-zA-ZÀ-ú]+)*\b$/;
+const emailRegex = /^(?!\\s*$)[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+cardBtn.addEventListener("click", () => {
+  card.classList.toggle("card-visible");
+  cardBtn.classList.toggle("rotate");
+});
+
+const confirmField = (input, regex) => {
+  if (!regex.test(input.value)) {
+    input.classList.remove("success");
+    input.classList.add("error");
+  } else {
+    input.classList.remove("error");
+    input.classList.add("success");
+  }
+};
+
+nameInput.addEventListener("change", () => confirmField(nameInput, nameRegex));
+
+emailInput.addEventListener("change", () =>
+  confirmField(emailInput, emailRegex)
+);
+
+submitForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  if (!emailRegex.test(emailInput.value) || !nameRegex.test(nameInput.value)) {
+    alert("Preencha os campos corretamente");
+    return;
+  }
+
+  const form = e.target;
+  const data = new FormData(form);
+
+  nameInput.value = "";
+  nameInput.classList.remove("success");
+  emailInput.value = "";
+  emailInput.classList.remove("success");
+  messageInput.value = "";
+
+  fetch("/formulario", {
+    method: "POST",
+    body: new URLSearchParams(data),
+  })
+    .then((resp) => resp.json())
+    .then((json) => console.log(JSON.stringify(json)))
+    .catch((e) => console.log(e));
+});
