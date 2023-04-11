fad2668456a42111986d8b9ad80e33e05ac7a084
Dionisio Diogenes <dionisiodiogenes@hotmail.com>
2023-04-11 17:40:19 -0300

    Instalação das dependências e criação do servidor local Node.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+app.use(express.static("."));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post("/formulario", (req, res) => {
+  res.send({
+    ...req.body,
+  });
+});
+
+app.listen(8080, () => console.log("Executando..."));
